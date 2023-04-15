63068b5bd85aee99db4ec5b97a8f60034789ace0
Ian Mobley <ianmobley@gmail.com>
2023-04-15 11:12:02 -0700

    refactor: change default layout
    Forgot arrow keys, added power regulator and some others

--- firmware/src/layout.rs
+++ firmware/src/layout.rs
@@ -10,19 +10,19 @@ pub const NUM_LAYERS: usize = 2;
 pub static LAYERS: Layers<NUM_COLS, NUM_ROWS, NUM_LAYERS> = layout! {
     {
         // TODO: adjust default lines
-        [ Escape   F1   F2   F3 F4 F5 F6    F7 F8 n F9   F10  F11 F12    ]
-        [ '`'      1    2    3  4  5  6     7  8  9 0    -    =   BSpace ]
-        [ Tab      Q    W    E  R  T  Y     U  I  O P    '['  ']' '\\'   ]
-        [ CapsLock A    S    D  F  G  H     J  K  L ;    '\'' n   Enter  ]
-        [ LShift   n    Z    X  C  V  B     N  M  , .    /    n   RShift ]
-        [ LCtrl    LGui LAlt n  n  n  Space n  n  n RAlt RGui (1) RCtrl  ]
+        [ Escape   F1   F2   F3 F4 F5       n  F6 F7   F8   F9   F10  F11   F12    ]
+        [ '`'      1    2    3  4  5        6  7  8    9    0    -    =     BSpace ]
+        [ Tab      Q    W    E  R  T        Y  U  I    O    P    '['  ']'   '\\'   ]
+        [ CapsLock A    S    D  F  G        H  J  K    L    ;    '\'' Enter n      ]
+        [ LShift   n    Z    X  C  V        B  N  M    ,    .    /    Up    n      ]
+        [ LCtrl    LGui LAlt n  n  Space    n  n  RAlt RGui (1)  Left Down  Right  ]
     }
     {
-        [ t t VolDown VolUp Mute MediaPlayPause MediaStop MediaPreviousSong MediaNextSong t t t t t ]
-        [ t t t       t     t    t              t         t                 t             t t t t t ]
-        [ t t t       t     t    t              t         t                 t             t t t t t ]
-        [ t t t       t     t    t              t         t                 t             t t t t t ]
-        [ t t t       t     t    t              t         t                 t             t t t t t ]
-        [ t t t       t     n    t              t         t                 t             t t t t t ]
+        [ t t VolDown VolUp Mute MediaPlayPause t t t t t t t t ]
+        [ t t t       t     t    t              t t t t t t t t ]
+        [ t t t       t     t    t              t t t t t t t t ]
+        [ t t t       t     t    t              t t t t t t t t ]
+        [ t t t       t     t    t              t t t t t t t t ]
+        [ t t t       t     n    t              t t t t t t t t ]
     }
 };
